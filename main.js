134591722792559f0750836007c4958aedf3398a
G3org3B <vbt.george@gmail.com>
2018-11-16 09:43:49 +0100

    fix

--- main.js
+++ main.js
@@ -6,26 +6,26 @@
 
 //CREARE VAR ARRAY CON DENTRO COGNOMI
 
-var Lista_cognomi = ["Bianchi", "Rossi", "Pippo", "Neri"];
-console.log([Lista_cognomi]);
+var lista_cognomi = ["Bianchi", "Rossi", "Pippo", "Neri"];
+console.log([lista_cognomi]);
 // PROMPT PER CHIEDERE COGNOME ALL'UTENTE
 
 var cognome_utente = prompt("Inserire il Cognome:")
 console.log(cognome_utente);
 // INSERIRE IL COGNOME UTENTE RICEVUTO NELL'ARRAY CON PUSH
 
-Lista_cognomi.push(cognome_utente);
-console.log([Lista_cognomi]);
+lista_cognomi.push(cognome_utente);
+console.log([lista_cognomi]);
 
 // CICLO DI CONTROLLO INSERIMENTO
- //for (var conteggio = 0; conteggio < Lista_cognomi.length; conteggio++) {
-  //console.log(Lista_cognomi[conteggio]);
+ //for (var conteggio = 0; conteggio < lista_cognomi.length; conteggio++) {
+  //console.log(lista_cognomi[conteggio]);
 // }
 
 //METTERE IN ORDINE ALFABETICA GLI ARRAY
 
-Lista_cognomi.sort()
-console.log(Lista_cognomi.sort());
+lista_cognomi.sort()
+console.log(lista_cognomi.sort());
 
 // INDIVIDUARE E STAMPARE LA POSIZIONE DEL UTENTE NELLA LISTA
 
@@ -34,10 +34,10 @@ var utente_trovato = false;
 var conteggio = 0;
 
 
-while (!utente_trovato && conteggio < Lista_cognomi.length) {
-  console.log((!utente_trovato && conteggio < Lista_cognomi.length));
-  if (cognome_utente == Lista_cognomi[conteggio]){
-    console.log(cognome_utente == Lista_cognomi[conteggio]);
+while (!utente_trovato && conteggio < lista_cognomi.length) {
+  console.log((!utente_trovato && conteggio < lista_cognomi.length));
+  if (cognome_utente == lista_cognomi[conteggio]){
+    console.log(cognome_utente == lista_cognomi[conteggio]);
     utente_trovato = true;
     console.log(utente_trovato = true);
   }
@@ -48,9 +48,9 @@ while (!utente_trovato && conteggio < Lista_cognomi.length) {
 }
 // STAMPARE LA LISTA UTENTI
 
-document.getElementById("Lista_cognomi").innerHTML = [Lista_cognomi];
+document.getElementById("Lista_cognomi").innerHTML = [lista_cognomi];
 
 
 // STAMPARE POSIZIONE UTENTE
-var position = Lista_cognomi.indexOf(cognome_utente)
+var position = lista_cognomi.indexOf(cognome_utente)
 document.getElementById('position').innerHTML = position;
